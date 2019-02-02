46939dbaad609202cda2bc7253e3e8525992da59
maximwwowpro <maxshvayuk@gmail.com>
2019-02-03 01:01:58 +0200

    Refactored FM receiver with FFT GUI. Now it uses inharritance from FM receiver WITHOUT GUI. Program completely works. Also add test.sh file - a script for FM receiver with FFT GUI testing. Deleted useless files. Updated README.

--- GNU_Radio_prototype/Examples/FM receiver/With FFT GUI/README.md
+++ GNU_Radio_prototype/Examples/FM receiver/With FFT GUI/README.md
@@ -1,6 +1,12 @@
 # FM receiver without graphical user interface
 Python 2.7 FM radio receiver program is presented here.  
-I have used **HackRF** SDR in my tests, but it should correctly work with anoyher SDRs, supported by **osmosdr** library.  
-You don't should have installed QT4 to run this program.  
-To start it, just enter `./fm_receiver_without_gui` in your terminal.  
+I have used **HackRF** SDR in my tests, but it should correctly work with anoyher SDRs, supported by **osmosdr** library (for example, **RTL SDR**).  
+You should have installed QT4 to run this program.  
+To start it, just enter `./test.sh` in your terminal.  
 GNU Radio packages should be installed to your Linux OS.  
+
+### Files description:  
+_test.sh_ - bash script with examples of commands how to launch a program. Run it to test!  
+_fm\_receiver\_with\_fft\_gui.grc_ - GNU Radio Companion scheme file with algorithm of FM receiver. You can use it to watch how does it work.  
+_fm_receiver_without_gui.py_ - this file contains **GR\_fm\_radio\_receiver\_without\_gui** - a class with basic FM receiver functional. It is a parent class for **GR\_fm\_radio\_receiver\_with\_fft\_gui**.  
+_fm\_receiver\_with\_fft\_gui.py_ - this file contains **GR\_fm\_radio\_receiver\_with\_fft\_gui** - a class which provides availability to run FM receiver with QT FFT GUI.
--- GNU_Radio_prototype/Examples/FM receiver/With FFT GUI/fm_receiver_with_fft_gui.py
+++ GNU_Radio_prototype/Examples/FM receiver/With FFT GUI/fm_receiver_with_fft_gui.py
@@ -5,37 +5,41 @@
 # There is GNU Radio Companion script with similar functionality, at the link. Use it to understand how this program works.
 # https://github.com/maximwowpro/RF-signal-power-meter
 # Kyiv, Ukraine.
-# 01.02.2019
+# 03.02.2019
 
 import sys, getopt
 from PyQt4 import Qt
-from gnuradio import analog
-from gnuradio import audio
-from gnuradio import blocks
-from gnuradio import filter
-from gnuradio import gr
 from gnuradio.filter import firdes
 import sip
-import osmosdr
 from gnuradio import qtgui
+from fm_receiver_without_gui import GR_fm_radio_receiver_without_gui
+from fm_receiver_without_gui import \
+	DEFAULT_SDR_SRC_HARDWARE_FREQ, \
+	DEFAULT_HETERODYNE_FREQ, \
+	DEFAULT_SAMPLE_RATE, \
+	DEFAULT_LOW_PASS_FILTER_CUTOFF_FREQ, \
+	DEFAULT_LOW_PASS_FILTER_TRANS_WIDTH, \
+	DEFAULT_OUTPUT_SIGNAL_GAIN
 
-DEFAULT_SDR_SRC_HARDWARE_FREQ = 100e6
-DEFAULT_HETERODYNE_FREQ = 0.5e6
-DEFAULT_SAMPLE_RATE = 4.8e6
-DEFAULT_LOW_PASS_FILTER_CUTOFF_FREQ = 70e3
-DEFAULT_LOW_PASS_FILTER_TRANS_WIDTH = 20e3
-DEFAULT_OUTPUT_SIGNAL_GAIN = 0.6
+QT_GUI_UPDATE_FREQUENCY = 0.01 #seconds
 
-class GR_fm_radio_receiver_with_fft_gui (gr.top_block):
+class GR_fm_radio_receiver_with_fft_gui (GR_fm_radio_receiver_without_gui):
 	def __init__(self, target_freq, samp_rate = DEFAULT_SAMPLE_RATE,
 				 low_pass_filter_cutoff_freq = DEFAULT_LOW_PASS_FILTER_CUTOFF_FREQ,
 				 low_pass_filter_trans_width = DEFAULT_LOW_PASS_FILTER_TRANS_WIDTH,
 				 volume = DEFAULT_OUTPUT_SIGNAL_GAIN,
-	             gui_mode = 0):
-		gr.top_block.__init__(self, "FM receiver with FFT GUI")
+				 gui_mode = 0):
 
-		if (1 == gui_mode):
-			print ("wceawceawc\nawceawceawe\necaweawcdca\n")
+		GR_fm_radio_receiver_without_gui.__init__(self,
+												  target_freq,
+												  samp_rate,
+												  low_pass_filter_cutoff_freq,
+												  low_pass_filter_trans_width,
+												  volume)
+
+		if (0 == gui_mode):
+			pass
+		elif (1 == gui_mode):
 			# Init QT GUI
 			self.qt_fft_gui = Qt.QWidget()
 			self.qt_fft_gui.setWindowTitle("FM receiver with FFT GUI")
@@ -60,80 +64,6 @@ class GR_fm_radio_receiver_with_fft_gui (gr.top_block):
 			self.qt_fft_gui.settings = Qt.QSettings("GNU Radio", "fm_receiver_with_fft_gui")
 			self.qt_fft_gui.restoreGeometry(self.qt_fft_gui.settings.value("geometry").toByteArray())
 
-		##################################################
-		# Variables
-		##################################################
-		self.wbfm_freq = 0
-		self.heterodyne_freq = 0
-		self.center_sdr_hardware_freq = 0
-		self.samp_rate = samp_rate
-		self.low_pass_filter_cutoff_freq = low_pass_filter_cutoff_freq
-		self.low_pass_filter_trans_width = low_pass_filter_trans_width
-		self.output_audio_gain = volume
-		if (0 == samp_rate):
-			self.samp_rate = DEFAULT_SAMPLE_RATE
-		if (0 == low_pass_filter_cutoff_freq):
-			self.low_pass_filter_cutoff_freq = DEFAULT_LOW_PASS_FILTER_CUTOFF_FREQ
-		if (0 == low_pass_filter_trans_width):
-			self.low_pass_filter_trans_width = DEFAULT_LOW_PASS_FILTER_TRANS_WIDTH
-		if (0 == volume):
-			self.output_audio_gain = DEFAULT_OUTPUT_SIGNAL_GAIN
-
-		self.calculate_frequencies (target_freq)
-
-		##################################################
-		# Blocks
-		##################################################
-		if ((self.audio_freq < 20e3) or (0 != self.samp_rate % self.wbfm_freq)):
-			self.samp_rate = DEFAULT_SAMPLE_RATE
-			self.wbfm_freq = DEFAULT_SAMPLE_RATE / 10
-			print ("Error! Sample rate % WBFM frequency should be == 0\n" +
-				   "Using default values: Sample rate = " + "%.1E"%(self.samp_rate) +
-				   " Hz\nWBFM frequency = " + "%.1E"%(self.wbfm_freq) + " Hz\n")
-
-		if ((self.audio_freq < 20e3) or (0 != self.wbfm_freq % self.audio_freq)):
-			self.wbfm_freq = self.samp_rate / 10
-			self.audio_freq = self.wbfm_freq / 10
-			print ("Error! WBFM frequency % audio final frequency should be == 0\n" +
-				   "Using default values:\nWBFM frequency = " + "%.1E"%(self.wbfm_freq) +
-				   " Hz\nAudio final frequency = " + "%.1E"%(self.audio_freq) + " Hz\n")
-
-		print ("Your FM radio settings:\nTarget frequency = " + "%.5g"%(target_freq) +
-			   " Hz\nSample rate = " + "%.3g"%(self.samp_rate) +
-			   " Hz \nLow pass filter cutoff frequency = " + "%.3g"%(self.low_pass_filter_cutoff_freq) +
-			   " Hz \nLow pass filter transition width = " + "%.3g"%(self.low_pass_filter_trans_width) +
-			   " Hz \nOutput signal gain (volume) = " + str(self.output_audio_gain) + "\n")
-
-		self.wbfm_receive = analog.wfm_rcv(quad_rate=self.wbfm_freq,
-										   audio_decimation=int(self.wbfm_freq / self.audio_freq),)
-
-		self.src_heterodyne = analog.sig_source_c(self.samp_rate, analog.GR_COS_WAVE, self.heterodyne_freq, 1, 0)
-
-		self.src_hackrf = osmosdr.source( args="numchan=" + str(1) + " " + '' )
-		self.src_hackrf.set_sample_rate(self.samp_rate)
-		self.src_hackrf.set_center_freq(self.center_sdr_hardware_freq, 0)
-		self.src_hackrf.set_freq_corr(0, 0)
-		self.src_hackrf.set_dc_offset_mode(0, 0)
-		self.src_hackrf.set_iq_balance_mode(0, 0)
-		self.src_hackrf.set_gain_mode(False, 0)
-		self.src_hackrf.set_gain(10, 0)
-		self.src_hackrf.set_if_gain(20, 0)
-		self.src_hackrf.set_bb_gain(30, 0)
-		self.src_hackrf.set_antenna('', 0)
-		self.src_hackrf.set_bandwidth(0, 0)
-
-		self.output_audio_final = audio.sink(int(self.audio_freq), '', True)
-
-		self.multiply_output_volume = blocks.multiply_const_vff((self.output_audio_gain, ))
-
-		self.low_pass_filter = filter.fir_filter_ccf(int(self.samp_rate / self.wbfm_freq),
-														firdes.low_pass(1, self.samp_rate,
-														self.low_pass_filter_cutoff_freq,
-														self.low_pass_filter_trans_width,
-														firdes.WIN_HAMMING, 6.76))
-		self.IF_mixer = blocks.multiply_vcc(1)
-
-		if (1 == gui_mode):
 			self.qt_gui_fft_sink = qtgui.freq_sink_c(
 				1024,                                                   # FFT array size
 				firdes.WIN_BLACKMAN_hARRIS,                             # wintype
@@ -142,7 +72,7 @@ class GR_fm_radio_receiver_with_fft_gui (gr.top_block):
 				"FM receiver spectre",                                  # Title of FFT sink
 				1                                                       # number of inputs
 			)
-			self.qt_gui_fft_sink.set_update_time(0.10)
+			self.qt_gui_fft_sink.set_update_time(QT_GUI_UPDATE_FREQUENCY)
 			self.qt_gui_fft_sink.set_y_axis(-140, 10)
 			self.qt_gui_fft_sink.set_y_label('Relative Gain', 'dB')
 			self.qt_gui_fft_sink.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
@@ -162,44 +92,17 @@ class GR_fm_radio_receiver_with_fft_gui (gr.top_block):
 			self.qt_gui_fft_sink.set_line_color(0, self.line_color)
 			self.qt_gui_fft_sink.set_line_alpha(0, self.line_transparrency)
 
+			# Create a wrapper of C++ GNU Radio API to display it's results in QWidget window. This is a magic, don't try to understand it.
 			self._qt_gui_fft_sink_win = sip.wrapinstance(self.qt_gui_fft_sink.pyqwidget(), Qt.QWidget)
 			self.top_grid_layout.addWidget(self._qt_gui_fft_sink_win)
 
-
-		##################################################
-		# Connections
-		##################################################
-		self.connect((self.IF_mixer, 0), (self.low_pass_filter, 0))
-		if (1 == gui_mode):
+			# Connect Intermediate Frequency mixer to QT GUI to build graph
 			self.connect((self.IF_mixer, 0), (self.qt_gui_fft_sink, 0))
-		self.connect((self.low_pass_filter, 0), (self.wbfm_receive, 0))
-		self.connect((self.multiply_output_volume, 0), (self.output_audio_final, 0))
-		self.connect((self.src_hackrf, 0), (self.IF_mixer, 1))
-		self.connect((self.src_heterodyne, 0), (self.IF_mixer, 0))
-		self.connect((self.wbfm_receive, 0), (self.multiply_output_volume, 0))
-
-		if (1 == gui_mode):
-			def closeEvent (self, event):
-				self.qt_gui_fft_sink.settings = Qt.QSettings("GNU Radio", "fm_receiver_with_fft_gui")
-				self.qt_gui_fft_sink.settings.setValue("geometry", self.qt_gui_fft_sink.saveGeometry())
-				event.accept()
-
-	def calculate_frequencies(self, target_freq):
-		self.wbfm_freq = self.samp_rate / 10
-		self.audio_freq = self.wbfm_freq / 10
-		#Target frequency = SDR hardware source frequency - Heterodyne frequency
-		#To understand this, read about Intermediate frequency and Heterodyning
-		if (DEFAULT_SDR_SRC_HARDWARE_FREQ == target_freq):
-			self.center_sdr_hardware_freq = DEFAULT_SDR_SRC_HARDWARE_FREQ + DEFAULT_HETERODYNE_FREQ
-			self.heterodyne_freq = DEFAULT_HETERODYNE_FREQ
 		else:
-			self.center_sdr_hardware_freq = DEFAULT_SDR_SRC_HARDWARE_FREQ
-			self.heterodyne_freq = DEFAULT_SDR_SRC_HARDWARE_FREQ - target_freq
+			print ("You set invalid GUI_mode (-g) option argument (\"" + str(gui_mode) + "\". \nEnter \"0\" to run without GUI or \"1\" to run with FFT GUI. \nNow progran starts without GUI.")
 
 
 def main (argv):
-	qapp = Qt.QApplication(sys.argv)
-
 	target_freq = 0
 	samp_rate = 0
 	low_pass_filter_cutoff_freq = 0
@@ -207,11 +110,21 @@ def main (argv):
 	volume = 0
 	gui_mode = 0
 
-	usage_str = "Usage: <script_name>.py -f <target Frequency> -s <Sample rate>(optional) -c <low pass filter Cutoff frequency>(optional) -t <low pass filter Trans width>(opyional) -v <audio volume>(optional) -g <Disable / Enable GUI: 0 or 1>(optional)\n"
+	usage_str = "Usage: <script_name>.py -f <target Frequency> -s <Sample rate>(optional) " + \
+				"-c <low pass filter Cutoff frequency>(optional) -t <low pass filter Trans width>(optional) " + \
+				"-v <audio volume>(optional) -g <Disable / Enable GUI: 0 or 1>(optional)\n"
 	if (0 == len(argv)):
 		print (usage_str)
 	try:
-		opts, args = getopt.getopt(argv,":h:f:s:c:t:v:g:",["targetfreq=", "samplerate=", "lowpasscutoff=", "lowpasstranswidth=", "volume=", "guimode="])
+		# Set short and long program arguments (flags)
+		opts, args = getopt.getopt(argv,
+								   ":h:f:s:c:t:v:g:",
+								   ["targetfreq=",
+									"samplerate=",
+									"lowpasscutoff=",
+									"lowpasstranswidth=",
+									"volume=",
+									"guimode="])
 	except getopt.GetoptError:
 		print (usage_str)
 		sys.exit(2)
@@ -237,28 +150,41 @@ def main (argv):
 		print ("Target freqency not defined, using default target frequency, " +
 			   "%.3E"%(target_freq) + " Hz\n")
 
-	fm_receiver = GR_fm_radio_receiver_with_fft_gui (float(target_freq), float(samp_rate),
-										float(low_pass_filter_cutoff_freq),
-										float(low_pass_filter_trans_width),
-										float(volume),
-	                                    int(gui_mode))
-	fm_receiver.start()
-	if ("1" == gui_mode):
-		fm_receiver.qt_fft_gui.show()
+	# To work with int number, not with str
+	gui_mode = int(gui_mode)
 
-		def stop_program ():
+	if ((0 == gui_mode) or (1 == gui_mode)):
+		if (1 == gui_mode):
+			qapp = Qt.QApplication(sys.argv)
+
+		fm_receiver = GR_fm_radio_receiver_with_fft_gui (float(target_freq), float(samp_rate),
+											float(low_pass_filter_cutoff_freq),
+											float(low_pass_filter_trans_width),
+											float(volume),
+											gui_mode)
+		if (1 == gui_mode):
+			print ("Run with FFT GUI.\n")
+			fm_receiver.start()
+			fm_receiver.qt_fft_gui.show()
+
+			def stop_program ():
+				fm_receiver.stop()
+				fm_receiver.wait()
+
+			qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), stop_program)
+			qapp.exec_()
+		elif (0 == gui_mode):
+			print ("Run without FFT GUI.\n")
+			fm_receiver.start()
+			try:
+				raw_input('Press Enter to quit: ')
+			except EOFError:
+				pass
 			fm_receiver.stop()
 			fm_receiver.wait()
-
-		qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), stop_program)
-		qapp.exec_()
 	else:
-		try:
-			raw_input('Press Enter to quit: ')
-		except EOFError:
-			pass
-		fm_receiver.stop()
-		fm_receiver.wait()
+		print ("You set invalid GUI_mode (-g) option argument (\"" + str(gui_mode) +
+			   "\". \nEnter \"0\" to run without GUI or \"1\" to run with FFT GUI. \nNow progran aborts.")
 
 
 if __name__ == '__main__':
